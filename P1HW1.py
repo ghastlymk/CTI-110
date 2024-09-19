ace736467f6435ead98122c43a5ed937c7fd0060
ghastlymk <landon.graessle@gmail.com>
2024-09-19 11:41:08 -0400

    Add files via upload

---
+++ P1HW1.py
@@ -0,0 +1,32 @@
+# Landon Graessle
+# 17 September 2024
+# P1HW1
+# Input and output with mathematical expressions
+
+print("-----Calculating Exponents-----")
+print()
+print()
+
+# base and exponents
+
+base = int(input("Enter an integer as a base value: "))
+power = int(input("Enter an integer as the exponent: "))
+
+# print output
+
+print()
+print("Result:", base ** power)
+print()
+print()
+print("-----Addition and Subtraction-----")
+
+# integers for adding and subtracting
+
+firstnumber = int(input("Enter your initial integer: "))
+secondnumber = int(input("Enter the integer to add: "))
+thirdnumber = int(input("Enter the integer to subtract: "))
+
+# print second output
+
+print()
+print("Result:", firstnumber + secondnumber - thirdnumber)
