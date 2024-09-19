407e5ec3f98aaa5a07a816f6e1701faab75d983a
ghastlymk <landon.graessle@gmail.com>
2024-09-19 10:31:08 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,16 @@
+# Landon Graessle
+# 17 September 2024
+# P1Lab1
+# Get input from user, display output
+
+# Get first name from user
+
+fname = input("Enter your first name here: ")
+
+# Get last name from user
+
+lname = input("Enter your last name here: ")
+
+# Print message with user input as output
+
+print("Welcome to CTI-110,", fname, lname + "!")
